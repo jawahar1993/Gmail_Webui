22487c25a2cee7b5e7813ebcd4a653a12ace46e8
Jawahar Lal <jawahar.ajg@gmail.com>
2020-09-13 22:29:24 +0530

    Refactored the code

--- src/test/java/com/jlal/pageobjects/UsernameSignInPage.java
+++ src/test/java/com/jlal/pageobjects/UsernameSignInPage.java
@@ -1,5 +1,6 @@
 package com.jlal.pageobjects;
 
+import com.jlal.util.WebUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,21 +11,19 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class UsernameSignInPage {
 
     public static void enterUsername(WebDriver driver, String s) {
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//input[@autocomplete='username']")));
-        WebElement usernameTextBox = driver.findElement(By.xpath("//input[@autocomplete='username']"));
-        usernameTextBox.clear();
-        usernameTextBox.sendKeys(s);
+        WebUtil.waitForElementVisibile(driver, By.xpath("//input[@autocomplete='username']"));
+
+        WebUtil.clearAndSendKeys(driver, By.xpath("//input[@autocomplete='username']"), s);
     }
 
     public static PasswordSignInPage clickUsernameNextButton(WebDriver driver) {
-        WebElement nextButton = driver.findElement(By.xpath("//div[@class='VfPpkd-RLmnJb']"));
-        nextButton.click();
+        WebUtil.click(driver, By.xpath("//div[@class='VfPpkd-RLmnJb']"));
 
         return PageFactory.initElements(driver, PasswordSignInPage.class);
     }
 
     public boolean isSignedOut(WebDriver driver) {
-        return driver.findElements(By.xpath("//div[text()='Signed out']")).size()>0;
+        return WebUtil.isElementExist(driver, By.xpath("//div[text()='Signed out']"));
+
     }
 }
--- src/test/java/com/jlal/util/WebUtil.java
+++ src/test/java/com/jlal/util/WebUtil.java
@@ -2,13 +2,38 @@ package com.jlal.util;
 
 import com.jlal.pageobjects.UsernameSignInPage;
 import com.jlal.pageobjects.UsernameSignInPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebUtil {
+    final static int wait_time_out = 30;
 
     public static UsernameSignInPage goToSignInPage(WebDriver driver) {
         driver.get("https://gmail.com");
         return PageFactory.initElements(driver, UsernameSignInPage.class);
+    }
+
+    public static void click(WebDriver driver, By xpath) {
+        WebElement element = driver.findElement(xpath);
+        element.click();
+    }
+
+    public static void waitForElementVisibile(WebDriver driver, By xpath) {
+        WebDriverWait wait = new WebDriverWait(driver, wait_time_out);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(xpath));
+    }
+
+    public static boolean isElementExist(WebDriver driver, By xpath) {
+        return driver.findElements(xpath).size()>0;
+    }
+
+    public static void clearAndSendKeys(WebDriver driver, By xpath, String s) {
+        WebElement elementTextBox = driver.findElement(xpath);
+        elementTextBox.clear();
+        elementTextBox.sendKeys(s);
     }
 }
