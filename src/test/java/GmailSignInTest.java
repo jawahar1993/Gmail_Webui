37afdf201eb3377a57c1922c6ca2429d7b51e9e1
Jawahar Lal <jawahar.ajg@gmail.com>
2020-09-13 14:54:57 +0530

    Script updation to support chrome launch and gmail ui

--- src/test/java/GmailSignInTest.java
+++ src/test/java/GmailSignInTest.java
@@ -1,5 +1,6 @@
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,35 +14,47 @@ import java.util.List;
 import java.util.Set;
 
 public class GmailSignInTest {
-    WebDriver driver = new FirefoxDriver();
+    public WebDriver driver;
+
+    @Before
+    public void startBrowser(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Thoughtpooladmin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
     @Test
     public void gmailValidLogin(){
         //1. Go to Gmail url
         driver.get("https://gmail.com");
         //2. Enter username
-        WebElement usernameTextBox = driver.findElement(By.id("Email"));
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//input[@autocomplete='username']")));
+        WebElement usernameTextBox = driver.findElement(By.xpath("//input[@autocomplete='username']"));
         usernameTextBox.clear();
         usernameTextBox.sendKeys("tuserfortest1@gmail.com");
-        //3. Enter password
-        WebElement passwordTextBox = driver.findElement(By.id("Passwd"));
+        //3. Click next button
+        WebElement usernameNextButton = driver.findElement(By.xpath("//div[@class='VfPpkd-RLmnJb']"));
+        usernameNextButton.click();
+        //4. Enter password
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//input[@autocomplete='current-password']")));
+        WebElement passwordTextBox = driver.findElement(By.xpath("//input[@autocomplete='current-password']"));
         passwordTextBox.clear();
         passwordTextBox.sendKeys("$tUserForTest1$");
-        //4. Click sign in
-        WebElement signInButton = driver.findElement(By.id("SignIn"));
-        signInButton.click();
-        //5. Verify that user signed in
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.partialLinkText("Inbox")));
-        Assert.assertTrue("Inbox should exist", driver.findElements(By.partialLinkText("Inbox")).size()>0);
-        //6. Click sign out
-        WebElement profileButton = driver.findElement(By.cssSelector("span[class='gb_da gbii']"));
+        //5. Click next button
+        WebElement passwordNextButton = driver.findElement(By.xpath("//div[@class='VfPpkd-RLmnJb']"));
+        passwordNextButton.click();
+        //6. Verify that user signed in
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//a[contains(@aria-label,'Inbox')]")));
+        Assert.assertTrue("Inbox should exist", driver.findElements(By.xpath("//a[contains(@aria-label,'Inbox')]")).size()>0);
+        //7. Click sign out
+        WebElement profileButton = driver.findElement(By.xpath("//img[@class='gb_La gbii']"));
         profileButton.click();
 
-        WebElement signOutButton = driver.findElement(By.id("gb_71"));
+        WebElement signOutButton = driver.findElement(By.xpath("//a[@id='gb_71']"));
         signOutButton.click();
-        //7. Verify that user signed out
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("signIn")));
-        Assert.assertTrue("SignIn button should exist", driver.findElements(By.id("signIn")).size()>0);
+        //8. Verify that user signed out
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[text()='Signed out']")));
+        Assert.assertTrue("SignedOut button should exist", driver.findElements(By.xpath("//div[text()='Signed out']")).size()>0);
     }
     @After
     public void tearDown(){
